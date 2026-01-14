541a40484acc13d8910cba4241a7d86aa90fe588
AndresAlv777 <151855872+AndresAlv777@users.noreply.github.com>
2026-01-14 01:47:30 -0500

    chore: backend deployed to Firebase Cloud Functions, ready for production

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
-import * as functions from "firebase-functions";
+import { onRequest } from "firebase-functions/v2/https";
 import app from "./app";
-import * as dotenv from "dotenv";
-dotenv.config();
 
-export const api = functions.https.onRequest(app);
+export const api = onRequest(
+  { cors: true },
+  app
+);
